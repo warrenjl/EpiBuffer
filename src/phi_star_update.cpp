e4d83ef4e5e5b44f0b2c001dd4a826867003626a
jlw98 <joshua.warren@yale.edu>
2025-11-21 15:07:53 -0500

    test

--- src/phi_star_update.cpp
+++ src/phi_star_update.cpp
@@ -156,85 +156,85 @@ for(int j = 0; j < n_grid; ++j){
    }
      
 //Center on the fly
-//phi_star = phi_star + 
-//           -mean(phi_star);
-//phi_tilde = C*(phi_star_corr_inv*phi_star);
-//arma::vec phi_tilde_full(n_ind); phi_tilde_full.fill(0.00);
-//for(int j = 0; j < n_ind; ++j){
-//   phi_tilde_full(j) = phi_tilde(v_index(j));
-//   }
-//radius_trans = (v_w)*gamma_old +
-//               phi_tilde_full;
-//Rcpp::NumericVector radius_trans_nv = Rcpp::NumericVector(radius_trans.begin(), 
-//                                                          radius_trans.end());
-//Rcpp::NumericVector radius_nv = Rcpp::pnorm(radius_trans_nv,
-//                                            0.00,
-//                                            1.00,
-//                                            true,
-//                                            false);
-//radius_nv = radius_nv*(radius_range(1) - radius_range(0)) + 
-//            radius_range(0); 
+phi_star = phi_star + 
+           -mean(phi_star);
+phi_tilde = C*(phi_star_corr_inv*phi_star);
+arma::vec phi_tilde_full(n_ind); phi_tilde_full.fill(0.00);
+for(int j = 0; j < n_ind; ++j){
+   phi_tilde_full(j) = phi_tilde(v_index(j));
+   }
+radius_trans = (v_w)*gamma_old +
+               phi_tilde_full;
+Rcpp::NumericVector radius_trans_nv = Rcpp::NumericVector(radius_trans.begin(), 
+                                                          radius_trans.end());
+Rcpp::NumericVector radius_nv = Rcpp::pnorm(radius_trans_nv,
+                                            0.00,
+                                            1.00,
+                                            true,
+                                            false);
+radius_nv = radius_nv*(radius_range(1) - radius_range(0)) + 
+            radius_range(0); 
 
-//radius = arma::vec(Rcpp::as<std::vector<double>>(radius_nv));
-//arma::mat radius_mat(n_ind, m); radius_mat.fill(0.00);
-//for(int j = 0; j < n_ind; ++ j){
-//   radius_mat.row(j).fill(radius(j));
-//   }
-//
+radius = arma::vec(Rcpp::as<std::vector<double>>(radius_nv));
+arma::mat radius_mat(n_ind, m); radius_mat.fill(0.00);
+for(int j = 0; j < n_ind; ++ j){
+   radius_mat.row(j).fill(radius(j));
+   }
+
 //Cumulative Counts
-//if(exposure_definition_indicator == 0){
-//  
-//  arma::umat comparison = ((v_exposure_dists) < radius_mat);
-//  arma::mat numeric_mat = arma::conv_to<arma::mat>::from(comparison);
-//  exposure = arma::sum(numeric_mat,
-//                       1);
-//  exposure = exposure/m_sd;
-//  
-//  }
-//
+if(exposure_definition_indicator == 0){
+  
+  arma::umat comparison = ((v_exposure_dists) < radius_mat);
+  arma::mat numeric_mat = arma::conv_to<arma::mat>::from(comparison);
+  exposure = arma::sum(numeric_mat,
+                       1);
+  exposure = exposure/m_sd;
+  
+  }
+
 //Spherical
-//if(exposure_definition_indicator == 1){
-//  
-//  arma::vec exposure_tmp(v_exposure_dists.n_rows, arma::fill::zeros);
-//  for(arma::uword i = 0; i < v_exposure_dists.n_rows; ++i){
-//    
-//     double sum_val = 0.0;
-//     for(arma::uword j = 0; j < v_exposure_dists.n_cols; ++j){
-//      
-//        double dist = v_exposure_dists(i,j);
-//        double rad = radius_mat(i,j);
-//      
-//        if(dist < rad){
-//        
-//          double fast = dist/rad;
-//          double fast3 = fast*fast*fast;
-//          sum_val += 1.0 - 1.5*fast + 0.5*fast3;
-//        
-//          }
-//      
-//        }
-//     exposure_tmp(i) = sum_val;
-//    
-//     }
-//  exposure = exposure_tmp/m_sd;
-//  
-//  }
-//
+if(exposure_definition_indicator == 1){
+  
+  arma::vec exposure_tmp(v_exposure_dists.n_rows, arma::fill::zeros);
+  for(arma::uword i = 0; i < v_exposure_dists.n_rows; ++i){
+    
+     double sum_val = 0.0;
+     for(arma::uword j = 0; j < v_exposure_dists.n_cols; ++j){
+      
+        double dist = v_exposure_dists(i,j);
+        double rad = radius_mat(i,j);
+      
+        if(dist < rad){
+        
+          double fast = dist/rad;
+          double fast3 = fast*fast*fast;
+          sum_val += 1.0 - 1.5*fast + 0.5*fast3;
+        
+          }
+      
+        }
+     exposure_tmp(i) = sum_val;
+    
+     }
+  exposure = exposure_tmp/m_sd;
+  
+  }
+
 //Presence/Absence
-//if(exposure_definition_indicator == 2){
-//  
-//  arma::umat comparison = ((v_exposure_dists) < radius_mat);
-//  arma::mat numeric_mat = arma::conv_to<arma::mat>::from(comparison);
-//  exposure = arma::max(numeric_mat,
-//                       1);
-//  exposure = exposure/m_sd;
-//  
-//  }
-//
-//for(int j = 0; j < p_q; ++j){
-//   Z.col(j) = exposure%v_q.col(j);
-//   } 
-//
+if(exposure_definition_indicator == 2){
+  
+  arma::umat comparison = ((v_exposure_dists) < radius_mat);
+  arma::mat numeric_mat = arma::conv_to<arma::mat>::from(comparison);
+  exposure = arma::max(numeric_mat,
+                       1);
+  exposure = exposure/m_sd;
+  
+  }
+
+for(int j = 0; j < p_q; ++j){
+   Z.col(j) = exposure%v_q.col(j);
+   } 
+
 return Rcpp::List::create(Rcpp::Named("phi_star") = phi_star,
                           Rcpp::Named("acctot_phi_star") = acctot_phi_star,
                           Rcpp::Named("radius") = radius,
